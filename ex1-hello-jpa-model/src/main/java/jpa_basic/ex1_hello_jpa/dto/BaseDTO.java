836748231f29d75f1e90b21ac31fae49c37bc288
unknown <braverokmc79@gmail.com>
2023-02-21 21:56:42 +0900

    10.  필드와 컬럼매핑

---
+++ ex1-hello-jpa-model/src/main/java/jpa_basic/ex1_hello_jpa/dto/BaseDTO.java
@@ -0,0 +1,7 @@
+package jpa_basic.ex1_hello_jpa.dto;
+
+/**
+ * 基本DTO字段
+ */
+public class BaseDTO {
+}
---
+++ ex1-hello-jpa-model/src/main/java/jpa_basic/ex1_hello_jpa/vo/BaseVO.java
@@ -0,0 +1,7 @@
+package jpa_basic.ex1_hello_jpa.vo;
+
+/**
+ * 基本VO字段
+ */
+public class BaseVO {
+}
